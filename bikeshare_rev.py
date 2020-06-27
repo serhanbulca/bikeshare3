76b045c8358aa3a30dd4634f1e0342753f35effa
TCIBULCA <N9IsbIsb>
2020-06-27 12:19:06 +0300

    feature:refactoring bikeshare for c to e 3

--- bikeshare_rev.py
+++ bikeshare_rev.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 # refactoring changes
 # commit 2
+# commit 3
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
